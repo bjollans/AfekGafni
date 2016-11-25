a591a87ac601a107ad9bbf0421c9c51ff802a257
Lidia <lidia.f.garces@gmail.com>
2016-11-25 10:30:11 +0100

    Created methods

--- src/DA_Process.java
+++ src/DA_Process.java
@@ -4,9 +4,11 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class DA_Process extends UnicastRemoteObject implements DA_Process_RMI{
 	
+	private final UUID id = UUID.randomUUID();
 	private Node node;
 	private int level = -1;
 	private static final long serialVersionUID = 6384248030531941625L;
@@ -16,17 +18,16 @@ public class DA_Process extends UnicastRemoteObject implements DA_Process_RMI{
 	private int[] vectorClock = new int[3];
 	private static final String NAMING = "proc";
 
-	protected DA_Process(int n, Node node){
+	protected DA_Process(int n) throws RemoteException{
 		super();
 		this.number = n;
-		this.node = node;
 	}
 
-	public void createProcesses(Node[] nodes) throws RemoteException{
+	public void createProcesses(ArrayList<String> addresses) throws RemoteException{
 		try {
-			rp = new DA_Process_RMI[ipAddresses.length];
+			rp = new DA_Process_RMI[addresses.size()];
 			for(int i=0; i<rp.length;i++){
-				rp[i]=(DA_Process_RMI)Naming.lookup();
+				rp[i]=(DA_Process_RMI)Naming.lookup(addresses.get(i));
 			}
 		} catch (MalformedURLException mue){
 			System.out.println("Your URL is malformed!");
@@ -34,31 +35,40 @@ public class DA_Process extends UnicastRemoteObject implements DA_Process_RMI{
 			long time = System.currentTimeMillis();
 			while(System.currentTimeMillis()-time <5000){}
 			System.out.println("polling...");
-			createProcesses(ipAddresses);
+			createProcesses(addresses);
 		}
 	}
 
-	public int sendMessage(String message) throws RemoteException {
+	public int sendID(String message) throws RemoteException {
 		System.out.println("\n");
-		System.out.println("SENDING MESSAGE");
+		System.out.println("SENDING ID to nodes");
 		
-		return false;
-	}
-	
-	private int[] getVectorClock() {
-		return vectorClock;
+		return 0;
 	}
 
-	public boolean receiveMessage(int level, Node foreignNode) throws RemoteException {
-		//TODO
+	public boolean receiveACK(int level, Node foreignNode) throws RemoteException {
+		System.out.println("\n");
+		System.out.println("RECEIVED ACK: process captured");
+		return false;
+	}
+	public int receiveMessage(String message) throws RemoteException {
+		System.out.println("\n");
+		System.out.println("RECEIVING ID from Candidate");
+		
+		return 0;
+	}
+	
+	public boolean sendACK(int level, Node foreignNode) throws RemoteException {
+		System.out.println("SEND ACK to Candidate");
 		return false;
 	}
 
 	private void setVectorClock(int [] newVector) {
 		this.vectorClock = newVector;
-		System.out.println("New global vector clock " + vectorToString(vectorClock));
-
-
+	}
+	
+	private int[] getVectorClock() {
+		return vectorClock;
 	}
 	
 }
