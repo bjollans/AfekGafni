d98125393da279aba742d838bdafd7d3c7dd211d
Lidia <lidia.f.garces@gmail.com>
2016-11-25 10:03:37 +0100

    Clean main class

--- src/DA_Process_main.java
+++ src/DA_Process_main.java
@@ -1,5 +1,6 @@
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public class DA_Process_main {
 
@@ -8,17 +9,22 @@ public class DA_Process_main {
 		int registryPort = Integer.parseInt(args[0]);
 		int processNumber = Integer.parseInt(args[1]);
 		
-		String ipAddress1 = args[2];
-		int rp1Number = Integer.parseInt(args[3]);
-		String ipAddress2 = args[4];
-		int rp2Number = Integer.parseInt(args[5]);
-		
+		ArrayList<Node> addresses = new ArrayList<Node>();
+
+		int processID = 1;
+		for (int i = 2; i < args.length; i++) {
+			if(processID==processNumber) processID++;
+			Node newNode = new Node(processID,args[i]);
+			addresses.add(newNode);
+			processID++;
+		}
+
 		try{
 			//
 			//server
 			//
 
-			System.setProperty("java.rmi.server.hostname","145.94.174.30");
+			System.setProperty("java.rmi.server.hostname","localhost");
 			try {
 				java.rmi.registry.LocateRegistry.createRegistry(registryPort);
 			} catch (RemoteException e){
@@ -27,32 +33,18 @@ public class DA_Process_main {
 				
 			}
 
-			DA_Process process1=new DA_Process();
+			DA_Process process1=new DA_Process(processNumber);
 
-			
-			Naming.rebind("rmi://145.94.174.30/"+"proc"+processNumber, process1);
-			process1.createProcesses(processNumber, ipAddress1, rp1Number, ipAddress2, rp2Number);
+			Naming.rebind("rmi://localhost/"+"proc"+processNumber, process1);
+			process1.createProcesses(addresses);
 			System.out.println("Server is Ready");
 			//
 			//client
 			//
 			System.out.println("Running process "+processNumber+".....");
-			System.out.println("Connected to process "+rp1Number+" and process "+rp2Number+".....");
-			Thread.sleep(10000);
+			System.out.println("Connected to processes");
 			System.out.println("Start to send");
-			boolean running = true;
-			while(running){
-				java.util.Random r = new java.util.Random();
-				int waitTime = (r.nextInt((5 - 1) + 1) + 1)*1000;
-				try {
-					Thread.sleep(waitTime);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				process1.sendMessage("time: "+(System.currentTimeMillis()-startTime)+" --Sender: proc"+processNumber);
-				//if (System.currentTimeMillis()==startTime+30000) running=false;
-			}
+			
 			
 
 		}catch(Exception e){
