9b8627a509138b52788d5bcce026385009021c21
Lidia <lidia.f.garces@gmail.com>
2016-11-26 14:32:59 +0100

    Added ordinaries array

--- src/DA_Process.java
+++ src/DA_Process.java
@@ -11,7 +11,8 @@ public class DA_Process extends UnicastRemoteObject implements DA_Process_RMI{
 	private final UUID id = UUID.randomUUID();
 	private UUID ownerID = id;
 	private Node node;
-	private int level = -1;
+	private int ordinaryLevel = -1;
+	private int candidateLevel = -1;
 	private static final long serialVersionUID = 6384248030531941625L;
 	public int number; 
 	private DA_Process_RMI[] rp;
@@ -20,6 +21,12 @@ public class DA_Process extends UnicastRemoteObject implements DA_Process_RMI{
 	private static final String NAMING = "proc";
 	private boolean elected = false;
 	private boolean ready= true;
+	private static final int EMPTYMSG = -1;
+	
+	private boolean candidateReady= true;
+	private boolean localOrdinaryReady= true;
+	
+	private boolean[] remoteOrdinariesReady;
 
 	protected DA_Process(int n) throws RemoteException{
 		super();
@@ -29,6 +36,7 @@ public class DA_Process extends UnicastRemoteObject implements DA_Process_RMI{
 	public void createProcesses(ArrayList<String> addresses) throws RemoteException{
 		try {
 			rp = new DA_Process_RMI[addresses.size()];
+			remoteOrdinariesReady = new boolean[addresses.size()];
 			for(int i=0; i<rp.length;i++){
 				rp[i]=(DA_Process_RMI)Naming.lookup(addresses.get(i));
 			}
@@ -44,6 +52,7 @@ public class DA_Process extends UnicastRemoteObject implements DA_Process_RMI{
 	
 	private boolean isOtherPartReady(){
 		//check if other part is readz, the two parts are candidate and ordinary part
+		//if other part is not ready, set own part to ready and return false
 		return false; //or true
 	}
 	
@@ -66,34 +75,48 @@ public class DA_Process extends UnicastRemoteObject implements DA_Process_RMI{
 	public int startCandidate() throws RemoteException {
 		System.out.println("\n");
 		System.out.println("START CANDIDATE PROCESS");
-		ArrayList<DA_Process_RMI> e = new ArrayList<DA_Process_RMI(Arrays.asList(rp));
+		ArrayList<DA_Process_RMI> e = new ArrayList<DA_Process_RMI>(java.util.Arrays.asList(rp));
+		int k =0;
 		while(true){
+			candidateLevel++;
 			synchronize();
-			if(level%2==0){
-				elected = true;
-				return 0;
+			int acks =0;
+			if(candidateLevel%2==0){
+				if(e.size()<1){
+					elected = true;
+					return 0;
+				}
+				else{
+					k = Math.min((int)Math.pow(2,candidateLevel/2),e.size());
+					ArrayList<DA_Process_RMI> e2 = new ArrayList<DA_Process_RMI>();
+					for(int i =0; i<k; i++){
+						e2.add(e.remove(0));
+					}
+					for(DA_Process_RMI proc: e2){
+						if(proc.startOrdinary(candidateLevel, id)==1){
+							acks++;
+						}
+					}
+				}
 			}
 			else{
-				int k = Math.min(Math.pow(2,level/2),e.size());
-				ArrayList<DA_Process_RMI> e2 = null;
+				if(acks<k) return 1;
 			}
 		}
-		
-		return 0;
 	}
 
 	public int startOrdinary(int candidateLevel, UUID candidateID) throws RemoteException {
 		System.out.println("\n");
 		System.out.println("START ORDINARY PROCESS");
 		
-		if (candidateLevel<level){
-			return 0;
+		if (candidateLevel<ordinaryLevel){
+			return -1;
 		}else{
 			if(idToLong(candidateID)<idToLong(ownerID)){
-				return 0;
+				return -1;
 			}
 			else{
-				level = candidateLevel;
+				ordinaryLevel = candidateLevel;
 				ownerID=candidateID;
 			}
 		}
